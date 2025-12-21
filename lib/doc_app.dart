455de99ac6dff06b2d817bc6ff01b896ff233207
ahmedgamalfouad155 <ahmedgamalfouad155@gmail.com>
2025-12-21 02:59:48 +0200

    add flutter svg and screenutil add navigation extiontions add routing folder and contains rout file add app text style add onboarding screen ui

---
+++ lib/doc_app.dart
@@ -0,0 +1,27 @@
+
+import 'package:docdoc/core/routing/app_router.dart';
+import 'package:docdoc/core/routing/routes.dart';
+import 'package:docdoc/core/theming/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DocApp extends StatelessWidget {
+  final AppRouter appRouter;
+  const DocApp({super.key, required this.appRouter}); 
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Doc Doc App', 
+        theme: ThemeData(
+          primaryColor: ColorsManager.mainBlue,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        initialRoute: Routes.onBoardingScreen,
+        onGenerateRoute: appRouter.generateRoute,
+      ),
+    );
+  }
+}
---
+++ lib/features/onboarding/get_started_button.dart
@@ -0,0 +1,28 @@
+import 'package:docdoc/core/helpers/extensions.dart';
+import 'package:docdoc/core/routing/routes.dart';
+import 'package:docdoc/core/theming/colors.dart';
+import 'package:docdoc/core/theming/styles.dart';
+import 'package:flutter/material.dart';
+
+class GetStartedButton extends StatelessWidget {
+  const GetStartedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        context.pushNamed(Routes.loginScreen);
+      },
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.all(ColorsManager.mainBlue),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        minimumSize: WidgetStateProperty.all(const Size(double.infinity, 50)),
+        shape: WidgetStateProperty.all(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        ),
+      ),
+
+      child: Text("Get Started", style: TextStyles.font16whitesemibold),
+    );
+  }
+}
